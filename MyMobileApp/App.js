0817d23517cd0f2fbbea0db409fab20c44459320
Bingusala <139640476+Bingusala@users.noreply.github.com>
2024-12-26 05:54:35 +0000

    Add state variables for amount, currencies, and converted amount

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -2,7 +2,18 @@ import React, { useState } from "react";
 import { StyleSheet, View, TextInput, Button, Text } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 
+
+
 export default function App() {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("LKR");
+  const [convertedAmount, setConvertedAmount] = useState("");
+  const [error, setError] = useState("");
+
+  const [fromCurrencyOpen, setFromCurrencyOpen] = useState(false);
+  const [toCurrencyOpen, setToCurrencyOpen] = useState(false);
+
   return <View style={styles.container}></View>;
 }
 
